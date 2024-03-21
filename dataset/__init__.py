c8529e65a020f24f982a33694bf594b989e2d0d1
ba-talibe <omarba2015@gmail.com>
2024-03-21 02:26:53 +0100

    added new fonction on package init file

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,3 +1,3 @@
 from .data_loading import update_dataset, load_dataset, add_counter_location, get_localisation_data
-from .data_functions import resample_data,counters_list, prepare_map_data, prepare_bar_data
-from .data_functions import frequencies, frequencies_to_column
+from .data_functions import resample_data,counters_list, prepare_map_data, prepare_bar_data, prepare_heatmap_data
+from .data_functions import frequencies, frequencies_to_column, heatmap_column
